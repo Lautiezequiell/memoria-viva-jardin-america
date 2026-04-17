e7804d9a81d6cadb28f40134bb6d036b301a85bb
Guerrero Lautaro Ezequiel <lauty9698@gmail.com>
2026-04-17 01:36:15 -0300

    Fix GitHub Pages deployment with proper basename and homepage

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/global.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter basename="/memoria-viva-jardin-america" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
